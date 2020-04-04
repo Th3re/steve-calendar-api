b5f263c3d3f1395513f003f688604acc0f1eaf26
Piotr Persona <persona.piotr@gmail.com>
2020-04-04 19:14:41 +0200

    Refactor environment
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- api/environment.py
+++ api/environment.py
@@ -1,66 +1,48 @@
 import os
 
 
-class Rabbit:
-    HOST_IN = 'RABBIT_HOST_IN'
-    EXCHANGE_IN = 'RABBIT_EXCHANGE_IN'
-    BINDING_KEY_IN = 'RABBIT_BINDING_KEY_IN'
-    HOST_OUT = 'RABBIT_HOST_OUT'
-    PORT_OUT = 'RABBIT_PORT_OUT'
-    CONNECTION_ATTEMPTS = 'RABBIT_CONNECTION_ATTEMPTS'
-    RETRY_DELAY = 'RABBIT_RETRY_DELAY'
-    EXCHANGE_OUT = 'RABBIT_EXCHANGE_OUT'
-    TOPIC_OUT = 'RABBIT_TOPIC_OUT'
-
-    def __init__(self, host_in, exchange_in, binding_key_in, host_out,
-                 port_out, connection_attempts, retry_delay, topic_out, exchange_out):
-        self.topic_out = topic_out
-        self.retry_delay = retry_delay
-        self.connection_attempts = connection_attempts
-        self.port_out = port_out
-        self.host_out = host_out
-        self.binding_key_in = binding_key_in
-        self.exchange_in = exchange_in
-        self.host_in = host_in
-        self.exchange_out = exchange_out
+class EnvClass:
+    def get(self, name: str) -> str:
+        env_name = f'{self.__class__.__name__.upper()}_{name.upper()}'
+        return os.environ[env_name]
 
     def __repr__(self):
-        return f'HOST_IN: {self.host_in} EXCHANGE_IN: {self.exchange_in} BINDING_KEY_IN: {self.binding_key_in} ' \
-               f'HOST_OUT {self.host_out} PORT_OUT: {self.port_out} CONNECTION_ATTEMPTS: {self.connection_attempts} ' \
-               f'RETRY_DELAY: {self.retry_delay} TOPIC_OUT: {self.topic_out} EXCHANGE_OUT: {self.exchange_out}'
+        return ' '.join([f'{self.__class__.__name__.upper()}_{name.upper()}: {value}'
+                         for name, value in self.__dict__.items()])
 
 
-class Google:
-    CLIENT_ID = 'CLIENT_ID'
-    CLIENT_SECRET = 'CLIENT_SECRET'
+class Rabbit(EnvClass):
+    def __init__(self):
+        super()
+        self.topic_out = self.get('topic_out')
+        self.retry_delay = int(self.get('retry_delay'))
+        self.connection_attempts = int(self.get('connection_attempts'))
+        self.port_out = self.get('port_out')
+        self.host_out = self.get('host_out')
+        self.binding_key_in = self.get('binding_key_in')
+        self.exchange_in = self.get('exchange_in')
+        self.host_in = self.get('host_in')
+        self.exchange_out = self.get('exchange_out')
 
-    def __init__(self, client_id, client_secret):
-        self.client_id = client_id
-        self.client_secret = client_secret
 
-    def __repr__(self):
-        return f'CLIENT_ID: {self.client_id} CLIENT_SECRET: {self.client_secret}'
+class Google(EnvClass):
+    def __init__(self):
+        super()
+        self.client_id = self.get('client_id')
+        self.client_secret = self.get('client_secret')
 
 
-class Environment:
+class Auth(EnvClass):
+    def __init__(self):
+        super()
+        self.url = self.get('url')
 
-    def __init__(self, google_credentials: Google, rabbit: Rabbit):
-        self.google_credentials = google_credentials
-        self.rabbit = rabbit
 
-    def __repr__(self):
-        return f'GOOGLE_CREDENTIALS: [{self.google_credentials}] RABBIT [{self.rabbit}]'
+class Environment(EnvClass):
+    def __init__(self):
+        self.google_credentials = Google()
+        self.rabbit = Rabbit()
 
 
 def read_environment() -> Environment:
-    return Environment(google_credentials=Google(client_id=os.environ[Google.CLIENT_ID],
-                                                 client_secret=os.environ[Google.CLIENT_SECRET]),
-                       rabbit=Rabbit(host_in=os.environ[Rabbit.HOST_IN],
-                                     exchange_in=os.environ[Rabbit.EXCHANGE_IN],
-                                     binding_key_in=os.environ[Rabbit.BINDING_KEY_IN],
-                                     host_out=os.environ[Rabbit.HOST_OUT],
-                                     port_out=os.environ[Rabbit.PORT_OUT],
-                                     connection_attempts=int(os.environ[Rabbit.CONNECTION_ATTEMPTS]),
-                                     retry_delay=int(os.environ[Rabbit.RETRY_DELAY]),
-                                     topic_out=os.environ[Rabbit.TOPIC_OUT],
-                                     exchange_out=os.environ[Rabbit.EXCHANGE_OUT]))
+    return Environment()
