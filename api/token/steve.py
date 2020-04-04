17c7fd16f0e578ec34eb25ec1de7932769afa3ef
Piotr Persona <persona.piotr@gmail.com>
2020-04-04 19:35:37 +0200

    Fetch token from Steve Auth
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- api/token/steve.py
+++ api/token/steve.py
@@ -1,6 +1,21 @@
+import logging
+
+import requests
+
 from api.token.service import TokenService, Token
 
 
+LOG = logging.getLogger(__name__)
+
+
 class SteveTokenService(TokenService):
+    def __init__(self, auth_url):
+        self.auth_url = auth_url
+
     def fetch(self, user_id: str) -> Token:
-        return Token(value='3xi4n23x42')
+        response = requests.get(f'{self.auth_url}/token/{user_id}')
+        token = response['token']
+        code = response['code']
+        message = response['message']
+        LOG.debug(f'Code: {code}, Message: {message}')
+        return Token(value=token)
